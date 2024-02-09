a6938e12ce8780c2dc33215662d2074cea05d309
rupalikiran1 <rupalikiran1999@gmail.com>
2024-02-09 15:48:04 +0530

    added

---
+++ day1.py
@@ -0,0 +1,83 @@
+x = 10
+print(x)
+x = 100
+print(x)
+
+# program 2
+# Arithmetic operation - 
+# Addition - +
+# Subtraction - -
+# Multiplication - *
+# Division - /
+# Remainder - %
+
+
+s = 9
+t = 3
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+q = 5
+r = 2
+
+print(q+r)
+print(q-r)
+print(q*r)
+print(q/r)
+print(q%r)
+
+
+
+def Calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+
+    Calculator(12,3)
+
+    # program 3
+
+    # function without parameter and without return type
+
+    def addA():
+        print(9+9)
+   
+    addA()
+
+
+    # function with parameter and without return type
+
+    def addB(x,y):
+        print(x+y)
+
+    addB(12,3)
+    addB(1,4)
+    addB(11,3)
+
+
+# function with parameter and with return type
+    
+    # 100 - shown
+    #100 - given
+
+    def addC(x,y):
+        return x + y
+    e = addC(12,5)
+    print(e)
+    print(e + e)
+    print(e * e)
+    
+
+
+
+
+
+
+
+
