ecd6849d256cf9b429b53553ed5eb804bd14b1a3
rupalikiran1 <rupalikiran1999@gmail.com>
2024-02-09 16:15:36 +0530

    added

---
+++ day2.py
@@ -0,0 +1,68 @@
+print("hello")
+
+
+# introvert   # extrovert
+# calm         loud
+# less outing    more outing
+# less social    more social
+
+# Human - 
+# Properties - weight , height ,color
+# Methods - walk() , talk()
+
+# vehicle
+# properties - color , regno, type, model
+# method - start() , stop()
+
+# Bank acc
+# properties - accNo , accType , bal , IFSC
+# Method - deposit() , withdrawl()
+
+
+a = 10
+print(a)
+print(type(a))
+# 10,-10,0
+
+b = 20.5
+print(b)
+print(type(b))
+# 12.5,-12.5,0.45
+
+c = True
+print(c)
+print(type(c))
+# True or false
+
+d = "krupali"
+print(d)
+print(type(d))
+# "A" , "a" ,'#' , "$$" , "fwer3456@#$" , " "
+
+# comparison operator
+# entity < entity ====> boolean ====>True or False
+# < , > , <= , >= , == , !=
+
+
+print(7 > 3)
+print(7 < 3)
+print(7 >= 3)
+print(7 <= 3)
+print(7 >= 7)
+print(7 <= 7)
+print(7 != 7)
+print(7 == 7)
+
+
+# logical operator
+
+# and
+# True and True  ====> True
+# False and True ===>False
+# True and False ====>False
+# False and False ===>False
+
+print(5 == 5 and 6 == 6)
+print(7 == 8 and 8 == 8)
+print(6 == 6 and 8 == 9)
+print(9 == 5 and 4 == 2)
