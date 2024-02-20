b1388d9254ad9a67366117443a0ac9385ad5f252
rupalikiran1 <rupalikiran1999@gmail.com>
2024-02-20 23:16:55 +0530

    added

--- day7.py
+++ day7.py
@@ -48,4 +48,59 @@ district = ["chandrapur","wardha","nagpur"]
 district.sort()
 print(district)
 
+# program 7
+listA = [11,22,33]
+#listB = listA
+# listB[0] = 88
+# print(listB)
+# print(listA)
 
+# copy()
+listD = listA.copy()
+listD[1] = 111
+print(listD)
+print(listA)
+
+# extend()
+listA = [11,22,33]
+listA.extend([77,88,99])
+print(listA)
+q1 = listA.index(77)
+print(q1)
+
+
+# program 1
+# append()
+birthyear = [1989,1990,1991,1992]
+ages = []
+for x in birthyear:
+    #print(2024-x)
+    ages.append(2024-x)
+    print(ages)
+
+# program 2
+marks = [1,2,33,45,66,77,33,44,65]
+above50 = []
+
+for x in marks:
+    if x > 50:
+        above50.append(x)
+        print(above50)
+
+ # program 3
+
+numbers = [11,22,33]
+total = 0
+for x in numbers:
+    total = total + x
+    #         0   + 11------>11
+    #         11   + 22------>33
+    #         33   + 33------->66
+    print(total)
+
+# program 4
+
+cities = ["pune","mumbai","banglore","chennai"]
+for x in cities:
+    print("welcome"+x)
+    
